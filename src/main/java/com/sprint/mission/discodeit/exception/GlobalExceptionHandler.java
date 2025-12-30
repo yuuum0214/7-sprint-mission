3ffa27d8f699e8b4dcfc038e5ca0f0472938e217
yumi Kim <cdcd0214@gmail.com>
2025-12-30 10:13:47 +0900

    스프린트6-7 : 주석제거

--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -16,28 +16,26 @@ public class GlobalExceptionHandler {
 
     // 400 Bad Request 반환
     @ExceptionHandler(IllegalArgumentException.class)
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<Map<String, String>> handleIllegalArgument(IllegalArgumentException e) {
-//        Map<String, String> response = new HashMap<>();
-//        response.put("message", e.getMessage());
-//        return response;
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("message", e.getMessage()));
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("message", e.getMessage()));
     }
 
     // 404 Not Fount 반환
     @ExceptionHandler(NoSuchElementException.class)
-//    @ResponseStatus(HttpStatus.NOT_FOUND) // 404
     public ResponseEntity<Map<String, String>> handleNotFound(NoSuchElementException e) {
-//        Map<String, String> response = new HashMap<>();
-//        response.put("message", e.getMessage());
-//        return response;
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("message", e.getMessage()));
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(Map.of("message", e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, Object>> handleException(Exception e) {
         log.error(e.getMessage(), e);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of(
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Map.of(
                 "status", 500,
                 "message", e.getMessage()
         ));
