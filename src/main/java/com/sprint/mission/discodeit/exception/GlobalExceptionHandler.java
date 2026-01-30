8e42dbf4bbd37709e6c1d045c4cc511c644855c3
yumi Kim <cdcd0214@gmail.com>
2026-01-30 15:19:39 +0900

    스프린트6-35 : Error 로직 수정

--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
@@ -16,10 +17,29 @@ public class GlobalExceptionHandler {
 
     // 400 Bad Request 반환
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<Map<String, String>> handleIllegalArgument(IllegalArgumentException e) {
+    public ResponseEntity<Map<String, Object>> handleIllegalArgumentException(IllegalArgumentException e) {
+        log.error("IllegalArgumentException: {}", e.getMessage());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", 400);
+        response.put("message", e.getMessage());
+
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(Map.of("message", e.getMessage()));
+                .body(response);
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<Map<String, Object>> handleIllegalStateException(IllegalStateException e) {
+        log.error("IllegalStateException: {}", e.getMessage());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", 404);
+        response.put("message", e.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(response);
     }
 
     // 404 Not Fount 반환
@@ -33,12 +53,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, Object>> handleException(Exception e) {
         log.error(e.getMessage(), e);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", 500);
+        response.put("message", e.getMessage());
+
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(Map.of(
-                "status", 500,
-                "message", e.getMessage()
-        ));
+                .body(response);
     }
 
 }
