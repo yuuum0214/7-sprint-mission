89e91dd4526e336f078a3d50f43bb7706cf5dcc7
yumi Kim <cdcd0214@gmail.com>
2026-02-24 11:40:57 +0900

    스프린트7-24 : User/Channel Repository 테스트 @EnableJpaAuditing추가

--- src/test/java/com/sprint/mission/discodeit/repository/ChannelRepositoryTest.java
+++ src/test/java/com/sprint/mission/discodeit/repository/ChannelRepositoryTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
@@ -18,6 +19,7 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @DataJpaTest
 @ActiveProfiles("test")
+@EnableJpaAuditing
 class ChannelRepositoryTest {
 
     @Autowired
